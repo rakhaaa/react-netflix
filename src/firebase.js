ee57b984610ca4a63a54e3d31c6e7aec73c82e60
rakhaaa <rakhaalifalmay25@gmail.com>
2022-11-11 08:18:09 +0700

    third

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyATmopYrgSAPQXMGQdmUnfBY9yxXxpCptY",
+  authDomain: "netflix-9adbf.firebaseapp.com",
+  projectId: "netflix-9adbf",
+  storageBucket: "netflix-9adbf.appspot.com",
+  messagingSenderId: "722137948614",
+  appId: "1:722137948614:web:37519b7d802b7721c692c7",
+  measurementId: "G-3YK5GSKVNW",
+};
