981234e1ac9f0e7f08f167d15109f3bd2d5e87fa
rakhaaa <rakhaalifalmay25@gmail.com>
2022-11-12 12:12:21 +0700

    sixth

--- src/Row.js
+++ src/Row.js
@@ -20,13 +20,11 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
     <div className="row">
       <h2>{title}</h2>
 
-      <div
-        className="row__posters"
-      >
+      <div className="row__posters">
         {movies.map((movie) => {
           return (
             <img
-              className="row__poster"
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
               key={movie.id}
               src={`${base_url}${
                 isLargeRow ? movie.poster_path : movie.backdrop_path
